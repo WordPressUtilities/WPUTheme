ecba47d5ad25bf3a9faf51857fd70734247e4846
Darklg <kev.rocher@gmail.com>
2014-12-03 12:50:06 +0100

    Activation - Set correct hook

--- inc/theme/activation.php
+++ inc/theme/activation.php
@@ -5,57 +5,60 @@
  * @package default
  */
 
-// Options
-$wputh_setup_options = array(
-    'date_format' => 'j F Y',
-    'permalink_structure' => '/%postname%/',
-    'timezone_string' => 'Europe/Paris',
-    'time_format' => 'H:i',
-    // Default values to avoid unnecessary queries
-    'widget_calendar' => '',
-    'widget_nav_menu' => '',
-    'widget_pages' => '',
-    'widget_post_categories' => '',
-    'widget_tag_cloud' => '',
-    'wpu_home_meta_description' => '',
-);
+add_action('switch_theme', 'wputh_setup_theme');
+function wputh_setup_theme() {
 
-$wputh_setup_pages = array(
-    'about__page_id' => array(
-        'post_title' => 'A Propos',
-        'post_content' => '<p>A Propos de ce site.</p>',
-    ),
-    'mentions__page_id' => array(
-        'post_title' => 'Mentions Legales',
-        'post_content' => '<p>Mentions l&eacute;gales du site.</p>',
-    ),
-);
+    // Options
+    $wputh_setup_options = array(
+        'date_format' => 'j F Y',
+        'permalink_structure' => '/%postname%/',
+        'timezone_string' => 'Europe/Paris',
+        'time_format' => 'H:i',
 
-// Old way for activation hook
-if ( is_admin() && isset( $_GET['activated'] ) && $pagenow == "themes.php" ) {
+        // Default values to avoid unnecessary queries
+        'widget_calendar' => '',
+        'widget_nav_menu' => '',
+        'widget_pages' => '',
+        'widget_post_categories' => '',
+        'widget_tag_cloud' => '',
+        'wpu_home_meta_description' => '',
+    );
+
+    $wputh_setup_pages = array(
+        'about__page_id' => array(
+            'post_title' => 'A Propos',
+            'post_content' => '<p>A Propos de ce site.</p>',
+        ) ,
+        'mentions__page_id' => array(
+            'post_title' => 'Mentions Legales',
+            'post_content' => '<p>Mentions l&eacute;gales du site.</p>',
+        ) ,
+    );
+
+
     // Setting options
-    foreach ( $wputh_setup_options as $name => $value ) {
-        update_option( $name, $value );
+    foreach ($wputh_setup_options as $name => $value) {
+        update_option($name, $value);
     }
 
     // Creating pages
-    foreach ( $wputh_setup_pages as $id => $page ) {
-        $option = get_option( $id );
+    foreach ($wputh_setup_pages as $id => $page) {
+        $option = get_option($id);
 
         // If page doesn't exists
-        if ( !is_numeric( $option ) ) {
+        if (!is_numeric($option)) {
 
-            if ( !isset( $page['post_status'] ) ) {
+            if (!isset($page['post_status'])) {
                 $page['post_status'] = 'publish';
             }
-            if ( !isset( $page['post_type'] ) ) {
+            if (!isset($page['post_type'])) {
                 $page['post_type'] = 'page';
             }
 
             // Create page
-            $option_page = wp_insert_post( $page );
-            if ( is_numeric( $option_page ) ) {
-                update_option( $id, $option_page );
+            $option_page = wp_insert_post($page);
+            if (is_numeric($option_page)) {
+                update_option($id, $option_page);
             }
         }
     }
