49bf203a42a4b27f46ec548d6166663e6791f5f2
Darklg <kev.rocher@gmail.com>
2014-12-26 12:23:40 +0100

    Add correct naming to Shortcode functions

--- inc/theme/shortcodes.php
+++ inc/theme/shortcodes.php
@@ -4,21 +4,21 @@
   [columns]Text on multiple columns[/columns]
 ---------------------------------------------------------- */
 
-function wpu_columns_shortcode( $atts, $content = null ) {
+function wputh_columns_shortcode($atts, $content = null) {
     return '<div class="post-content-columns">' . $content . '</div>';
 }
-add_shortcode( 'columns', 'wpu_columns_shortcode' );
+add_shortcode('columns', 'wputh_columns_shortcode');
 
 /* ----------------------------------------------------------
   [googlemap]8 Rue de Londres, 75009 Paris, France[/googlemap]
 ---------------------------------------------------------- */
 
-function wpu_googlemap_shortcode( $atts, $content = null ) {
-    $width = isset( $atts['width'] ) ? $atts['width'] : 640;
-    $height = isset( $atts['height'] ) ? $atts['height'] : 480;
-    return '<iframe width="'.$width.'" height="'.$height.'" src="http://maps.google.com/maps?q='.urlencode( $content ).'&output=embed"></iframe>';
+function wputh_googlemap_shortcode($atts, $content = null) {
+    $width = isset($atts['width']) ? $atts['width'] : 640;
+    $height = isset($atts['height']) ? $atts['height'] : 480;
+    return '<iframe width="' . $width . '" height="' . $height . '" src="http://maps.google.com/maps?q=' . urlencode($content) . '&output=embed"></iframe>';
 }
-add_shortcode( "googlemap", "wpu_googlemap_shortcode" );
+add_shortcode("googlemap", "wputh_googlemap_shortcode");
 
 /* ----------------------------------------------------------
   [widget type="WP_Widget_Recent_Posts"]
@@ -26,27 +26,24 @@ add_shortcode( "googlemap", "wpu_googlemap_shortcode" );
 
 /* Thx http://wp.smashingmagazine.com/2012/12/11/inserting-widgets-with-shortcodes/ */
 
-add_shortcode( 'widget', 'wpu_widget_shortcode' );
-function wpu_widget_shortcode( $atts ) {
+add_shortcode('widget', 'wputh_widget_shortcode');
+function wputh_widget_shortcode($atts) {
 
     // Configure defaults and extract the attributes into variables
-    extract( shortcode_atts(
-            array(
-                'type'  => '',
-                'title' => '',
-            ),
-            $atts
-        ) );
+    extract(shortcode_atts(array(
+        'type' => '',
+        'title' => '',
+    ) , $atts));
 
     $args = array(
         'before_widget' => '<div class="box widget">',
-        'after_widget'  => '</div>',
-        'before_title'  => '<div class="widget-title">',
-        'after_title'   => '</div>',
+        'after_widget' => '</div>',
+        'before_title' => '<div class="widget-title">',
+        'after_title' => '</div>',
     );
 
     ob_start();
-    the_widget( $type, $atts, $args );
+    the_widget($type, $atts, $args);
     return ob_get_clean();
 }
 
@@ -54,7 +51,7 @@ function wpu_widget_shortcode( $atts ) {
   [get_site_option key="option_name"] Get option
 ---------------------------------------------------------- */
 
-add_shortcode('get_site_option', 'fun_get_site_option');
-function fun_get_site_option($atts) {
+add_shortcode('get_site_option', 'wputh_get_site_option');
+function wputh_get_site_option($atts) {
     return get_option($atts['key']);
 }
