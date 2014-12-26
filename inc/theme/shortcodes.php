772ecde67b4194adb1406490874cc19860105508
Darklg <kev.rocher@gmail.com>
2014-12-26 12:24:29 +0100

    Add video shortcode and move Shortcode CSS to a separate sheet.

--- inc/theme/shortcodes.php
+++ inc/theme/shortcodes.php
@@ -55,3 +55,35 @@ add_shortcode('get_site_option', 'wputh_get_site_option');
 function wputh_get_site_option($atts) {
     return get_option($atts['key']);
 }
+
+/* ----------------------------------------------------------
+  [responsive_youtube]https://www.youtube.com/watch?v=wl2Rc4R0yLQ[/responsive_youtube]
+---------------------------------------------------------- */
+
+add_shortcode('responsive_youtube', 'wputh_youtube_shortcode');
+function wputh_youtube_shortcode($atts, $content = "") {
+    $url = trim($content);
+
+    // Check URL
+    if (filter_var($url, FILTER_VALIDATE_URL) === FALSE) {
+        return;
+    }
+
+    // Extract URL params
+    $url_details = parse_url($url);
+
+    // Check query
+    if (!isset($url_details['query'])) {
+        return;
+    }
+
+    // Extract query params
+    parse_str($url_details['query'], $arr);
+
+    // Check "v" param
+    if (!isset($arr['v'])) {
+        return;
+    }
+
+    return '<div class="wputh-video-container"><iframe src="http://www.youtube.com/embed/' . $arr['v'] . '" height="315" width="560" allowfullscreen="" frameborder="0"></iframe></div>';
+}
