e1aac467cce9f1ef5045b4eb50e3b7eb678bb28b
Darklg <kev.rocher@gmail.com>
2014-12-31 15:16:48 +0100

    Z-Protect : Load a default 404 page

--- z-protect.php
+++ z-protect.php
@@ -1,5 +1,21 @@
 <?php
-if ( !defined( 'TEMPLATEPATH' ) ) {
-    header( $_SERVER["SERVER_PROTOCOL"]." 404 Not Found" );
-    exit( 'Error' );
+if (defined('TEMPLATEPATH') || defined('WPUTH_IS_Z_PROTECT')) {
+    return;
 }
+define('WPUTH_IS_Z_PROTECT', true);
+
+// Load base WordPress file
+$bootfile = 'wp-load.php';
+while (!is_file($bootfile)) {
+    if (is_dir('..')) {
+        chdir('..');
+    } else {
+        exit('Oups');
+    }
+}
+require_once ($bootfile);
+
+// Load 404
+header( $_SERVER["SERVER_PROTOCOL"]." 404 Not Found" );
+include get_404_template();
+die;
