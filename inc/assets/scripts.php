701936c2ff06ac4d49a7a7b39365460663b80aa9
Darklg <kev.rocher@gmail.com>
2015-01-13 21:07:57 +0100

    v 1.3.0 — Scripts are loaded via a filter / Default jQuery migrate is removed

--- inc/assets/scripts.php
+++ inc/assets/scripts.php
@@ -1,40 +1,65 @@
 <?php
 include dirname(__FILE__) . '/../../z-protect.php';
 
+/* ----------------------------------------------------------
+  Remove JQ Migrate
+---------------------------------------------------------- */
+
+add_filter('wp_default_scripts', 'wputh_disable_jqmigrate');
+
+/* http://subinsb.com/remove-jquery-migrate-in-wp-blog */
+function wputh_disable_jqmigrate(&$scripts) {
+    if (!is_admin()) {
+        $scripts->remove('jquery');
+        $scripts->add('jquery', false, array(
+            'jquery-core'
+        ) , '1.11.1');
+    }
+}
+
+/* ----------------------------------------------------------
+  Add JS
+---------------------------------------------------------- */
+
+add_filter('wputh_javascript_files', 'wputh_default_wputh_javascript_files', 1, 1);
+function wputh_default_wputh_javascript_files($scripts = array()) {
+    $scripts['mootools'] = array(
+        'uri' => '/js/lib/mootools-core-1.4.5-full-nocompat-yc.js'
+    );
+    $scripts['mootools-more'] = array(
+        'uri' => '/js/lib/mootools-more-1.4.0.1.js'
+    );
+    $scripts['dk-smooth-scroll'] = array(
+        'uri' => '/js/classes/dk-smooth-scroll.js',
+        'footer' => 1
+    );
+    $scripts['functions'] = array(
+        'uri' => '/js/functions.js',
+        'footer' => 1
+    );
+    $scripts['wpu-home'] = array(
+        'uri' => '/js/modules/home.js',
+        'footer' => 1
+    );
+    $scripts['wpu-faq'] = array(
+        'uri' => '/js/modules/faq.js',
+        'footer' => 1
+    );
+    $scripts['events'] = array(
+        'uri' => '/js/events.js',
+        'footer' => 1
+    );
+    return $scripts;
+}
+
 function wputh_add_javascripts() {
+    $scripts = apply_filters('wputh_javascript_files', array());
     global $WPUJavaScripts;
-    if (!isset($WPUJavaScripts) || !is_array($WPUJavaScripts)) {
-        $WPUJavaScripts = array(
-            'mootools' => array(
-                'uri' => '/js/lib/mootools-core-1.4.5-full-nocompat-yc.js'
-            ) ,
-            'mootools-more' => array(
-                'uri' => '/js/lib/mootools-more-1.4.0.1.js'
-            ) ,
-            'dk-smooth-scroll' => array(
-                'uri' => '/js/classes/dk-smooth-scroll.js',
-                'footer' => 1
-            ) ,
-            'functions' => array(
-                'uri' => '/js/functions.js',
-                'footer' => 1
-            ) ,
-            'wpu-home' => array(
-                'uri' => '/js/modules/home.js',
-                'footer' => 1
-            ) ,
-            'wpu-faq' => array(
-                'uri' => '/js/modules/faq.js',
-                'footer' => 1
-            ) ,
-            'events' => array(
-                'uri' => '/js/events.js',
-                'footer' => 1
-            )
-        );
+    if (isset($WPUJavaScripts) && is_array($WPUJavaScripts)) {
+        $scripts = $WPUJavaScripts;
     }
 
-    foreach ($WPUJavaScripts as $id => $details) {
+    foreach ($scripts as $id => $details) {
         $url = '';
         if (!isset($details['uri']) && !isset($details['url'])) {
             wp_enqueue_script($id);
