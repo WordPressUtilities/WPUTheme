77e5b47a6691ac6fb204fc7cb323edb723e20dc4
Darklg <kev.rocher@gmail.com>
2015-03-31 23:49:12 +0200

    v 1.6.2
    * Fix HTML Search form

--- tpl/header/searchform.php
+++ tpl/header/searchform.php
@@ -5,8 +5,8 @@ include dirname( __FILE__ ) . '/../../z-protect.php';
   <form role="search" method="get" id="header-search" class="search__form" action="<?php echo home_url(); ?>">
       <div class="search__inner">
           <label class="cssc-remove-element" for="s"><?php _e( 'Search for:', 'wputh' ); ?></label>
-          <input type="text" value="" name="s" id="s" class="search__input" id="header-search__input" placeholder="<?php echo esc_attr__( 'Enter your keywords...', 'wputh' ); ?>" title="<?php echo esc_attr__( 'Search by keywords', 'wputh' ); ?>" />
-          <button class="search__submit cssc-button cssc-button--default" id="search_submit" title="<?php echo sprintf( __( 'Search on %s', 'wputh' ), get_bloginfo('name') ); ?>"><?php _e('Search', 'wputh'); ?></button>
+          <input type="text" value="" name="s" id="s" class="search__input" placeholder="<?php echo esc_attr__( 'Enter your keywords...', 'wputh' ); ?>" title="<?php echo esc_attr__( 'Search by keywords', 'wputh' ); ?>" />
+          <button type="submit" class="search__submit cssc-button cssc-button--default" id="search_submit" title="<?php echo sprintf( __( 'Search on %s', 'wputh' ), get_bloginfo('name') ); ?>"><?php _e('Search', 'wputh'); ?></button>
       </div>
   </form>
 </div>
