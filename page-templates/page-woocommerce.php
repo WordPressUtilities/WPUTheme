4d05f7bb3a2044eb345882b7e83818036d1dbc03
Darklg <kev.rocher@gmail.com>
2015-10-05 17:12:14 +0200

    v 2.9
    * Add a distinct template for Woocommerce pages

--- page-templates/page-woocommerce.php
+++ page-templates/page-woocommerce.php
@@ -1,10 +1,15 @@
 <?php
-include dirname( __FILE__ ) . '/z-protect.php';
+
+/* Template Name: Woocommerce */
+
+include dirname(__FILE__) . '/../z-protect.php';
+
+the_post();
 get_header();
 ?>
 <div class="main-content">
 <?php do_action('woocommerce_before_main_content'); ?>
-<?php woocommerce_content(); ?>
+<?php the_content(); ?>
 <?php do_action('woocommerce_after_main_content'); ?>
 </div>
 <?php
