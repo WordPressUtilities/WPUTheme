368c92831ed4b5a5740ec962a28cfd924ec3ed5d
Darklg <kev.rocher@gmail.com>
2016-01-25 22:06:06 +0100

    v 2.25
    * Add a hook for JS Values.

--- tpl/header/jsvalues.php
+++ tpl/header/jsvalues.php
@@ -1,1 +1,12 @@
-<div id="js-values" class="js-values" data-page_title="<?php echo addslashes( trim( wp_title( ) ) ); ?>" data-body_<?php body_class( 'cssc-is-responsive' ); ?>></div>
+<?php
+$js_values_html_attributes = apply_filters('wputh_jsvalues', array(
+    'page_title' => addslashes(trim(wp_title(null, false))),
+    'body_class' => implode(' ', get_body_class('cssc-is-responsive'))
+));
+$html_jsvalues = '';
+foreach ($js_values_html_attributes as $id => $value) {
+    $html_jsvalues .= ' data-' . $id . '="' . esc_attr($value) . '"';
+}
+
+?>
+<div id="js-values" class="js-values" <?php echo $html_jsvalues; ?>></div>
