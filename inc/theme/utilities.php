e5b05f002732ce60bfe10a9251211af0b218b4c6
Kevin <kevin@rocher-dev.com>
2017-10-20 19:08:46 +0200

    v 2.43.0
    * Utilities : Get all user values for a meta.

--- inc/theme/utilities.php
+++ inc/theme/utilities.php
@@ -642,3 +642,17 @@ function wputh_update_without_revision($args = array()) {
     add_action('pre_post_update', 'wp_save_post_revision');
     return $update_action;
 }
+
+/* ----------------------------------------------------------
+  Get all user values for a meta
+---------------------------------------------------------- */
+
+function wputh_get_all_users_values_for($meta_key = '') {
+    global $wpdb;
+    $_users = $wpdb->get_results($wpdb->prepare("SELECT user_id, meta_value FROM {$wpdb->usermeta} WHERE meta_key = %s", $meta_key));
+    $users = array();
+    foreach ($_users as $_user) {
+        $users[$_user->user_id] = $_user->meta_value;
+    }
+    return $users;
+}
