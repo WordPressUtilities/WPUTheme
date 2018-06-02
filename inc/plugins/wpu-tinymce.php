36f601bfb1f1e36bf09d9f3ad974b5c06ea97494
Kevin <kevin@rocher-dev.com>
2018-06-02 18:09:50 +0200

    v 2.48.1.1
    * Fix image table dir

--- inc/plugins/wpu-tinymce.php
+++ inc/plugins/wpu-tinymce.php
@@ -8,7 +8,7 @@ add_filter('wputinymce_buttons', 'wputh_set_wputinymce_buttons__insert_table');
 function wputh_set_wputinymce_buttons__insert_table($buttons) {
     $buttons['insert_table'] = array(
         'title' => 'Insert a table',
-        'image' => get_stylesheet_directory_uri() . '/images/tinymce/table.png',
+        'image' => get_template_directory_uri() . '/images/tinymce/table.png',
         'html' => '<table class="wputinymce-table"><thead><tr><th>Heading</th><th>Heading</th></tr></thead><tbody><tr><td>Content</td><td>Content</td></tr></tbody></table>'
     );
     return $buttons;
