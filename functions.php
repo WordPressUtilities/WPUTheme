5798e7e94b935918d04ae998b13221d02e90ac95
Kevin <kevin@rocher-dev.com>
2019-04-09 18:22:40 +0200

    v 2.57.3
    Forgotten Password Template :
    * Add hooks.
    * Default page if user is logged in.

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 require_once dirname(__FILE__) . '/z-protect.php';
 
-define('WPUTHEME_VERSION','2.57.2');
+define('WPUTHEME_VERSION','2.57.3');
 
 if (apply_filters('wputheme_usesessions', true)) {
     @session_start();
--- page-templates/page-forgottenpassword.php
+++ page-templates/page-forgottenpassword.php
@@ -1,6 +1,12 @@
 <?php
 /* Template Name: Forgotten Password */
 include dirname(__FILE__) . '/../z-protect.php';
+
+if (is_user_logged_in()) {
+    wp_redirect(apply_filters('wputh_forgottenpassword_url_logged_in', site_url()));
+    die;
+}
+
 get_header();
 the_post();
 
@@ -17,15 +23,18 @@ $message_success = apply_filters('wputh_forgottenpassword_message', __('Check yo
             </ul>
         </div>
     <?php endif;?>
+    <?php do_action('wputh_forgottenpassword_before_form'); ?>
     <form name="lostpasswordform" id="lostpasswordform" action="<?php echo esc_url(network_site_url('wp-login.php?action=lostpassword', 'login_post')); ?>" method="post">
         <p>
             <label for="user_login" ><?php _e('Username or Email Address');?><br />
             <input type="text" name="user_login" id="user_login" class="input" value="" size="20" autocapitalize="off" /></label>
         </p>
         <?php do_action('lostpassword_form');?>
+        <?php do_action('wputh_forgottenpassword_form');?>
         <input type="hidden" name="redirect_to" value="<?php echo esc_attr($redirect_to); ?>" />
         <p class="submit"><input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="<?php esc_attr_e('Get New Password');?>" /></p>
     </form>
+    <?php do_action('wputh_forgottenpassword_after_form'); ?>
 </article>
 </div>
 <?php
