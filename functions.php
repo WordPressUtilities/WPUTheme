7dfe31fdbf5b8d7135552f4cbede9256264186e2
Kevin <kevin@rocher-dev.com>
2019-04-22 23:57:47 +0200

    v 2.57.6
    * Header : restrict number of datalist elements + remove a query on terms.

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 require_once dirname(__FILE__) . '/z-protect.php';
 
-define('WPUTHEME_VERSION','2.57.5');
+define('WPUTHEME_VERSION','2.57.6');
 
 if (apply_filters('wputheme_usesessions', true)) {
     @session_start();
--- tpl/header/searchform.php
+++ tpl/header/searchform.php
@@ -1,26 +1,29 @@
 <?php
+$_min_count = apply_filters('wputh_searchform_datalist_mincount', 3);
 include dirname(__FILE__) . '/../../z-protect.php';
 ?>
 <div class="search search--header header-search">
   <form role="search" method="get" id="header-search" class="search__form" action="<?php echo home_url(); ?>">
       <div class="search__inner">
-          <label class="cssc-remove-element" for="s"><?php _e('Search for:', 'wputh'); ?></label>
+          <label class="cssc-remove-element" for="s"><?php _e('Search for:', 'wputh');?></label>
           <input list="header-search-list" type="text" value="" name="s" id="s" class="search__input" placeholder="<?php echo esc_attr__('Enter your keywords...', 'wputh'); ?>" title="<?php echo esc_attr__('Search by keywords', 'wputh'); ?>" />
-          <button type="submit" class="search__submit cssc-button cssc-button--default" id="search_submit" title="<?php echo sprintf(__('Search on %s', 'wputh') , get_bloginfo('name')); ?>"><?php _e('Search', 'wputh'); ?></button>
+          <button type="submit" class="search__submit cssc-button cssc-button--default" id="search_submit" title="<?php echo sprintf(__('Search on %s', 'wputh'), get_bloginfo('name')); ?>"><?php _e('Search', 'wputh');?></button>
       </div>
   </form>
 </div>
 <?php
 $all_terms = array();
-$terms_type = array(
-    'post_tag',
-    'category'
-);
-foreach ($terms_type as $term) {
-    $terms = get_terms($term);
-    foreach ($terms as $item) {
-        $all_terms[$item->slug] = $item->name;
+$terms = get_terms(array(
+    'taxonomy' => array(
+        'post_tag',
+        'category'
+    )
+));
+foreach ($terms as $item) {
+    if ($item->count < $_min_count) {
+        continue;
     }
+    $all_terms[$item->slug] = $item->name;
 }
 asort($all_terms);
 
