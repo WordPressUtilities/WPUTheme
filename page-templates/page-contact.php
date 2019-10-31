2c025c160b67b5f99ba3809b07d3bc6530af5f9f
Kevin <kevin@rocher-dev.com>
2019-10-31 20:52:25 +0100

    v 2.62.0
    * Contact : Add correct form layout.
    * Editor : Add basic layout for table & blockquote.

--- page-templates/page-contact.php
+++ page-templates/page-contact.php
@@ -1,14 +1,14 @@
 <?php
 /* Template Name: Contact */
-include dirname( __FILE__ ) . '/../z-protect.php';
+include dirname(__FILE__) . '/../z-protect.php';
 get_header();
 the_post();
 ?>
 <div class="main-content">
 <article>
-    <h1><?php the_title(); ?></h1>
-    <?php the_content(); ?>
-    <?php do_action('wpucontactforms_content'); ?>
+    <h1><?php the_title();?></h1>
+    <?php the_content();?>
+    <?php do_action('wpucontactforms_content', false, 'default');?>
 </article>
 </div>
 <?php
