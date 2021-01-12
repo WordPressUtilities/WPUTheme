8029c1e05d0fbabe632c25ef9cece634191bc818
Kevin <kevin@rocher-dev.com>
2021-01-12 18:45:27 +0100

    v 2.84.3
    * Fix smooth scroll.

--- js/functions/smooth-scroll.js
+++ js/functions/smooth-scroll.js
@@ -5,13 +5,13 @@
 var wpu_set_smooth_scroll = function() {
     /* Scrollto */
     jQuery('[href^="#"]:not(.no-smooth)').on('click', function(e) {
-        e.preventDefault();
         var href = jQuery(this).attr('href');
         if (href == '#') {
             return;
         }
         var $href = jQuery(href);
         if ($href.length > 0) {
+            e.preventDefault();
             jQuery('html,body').animate({
                 scrollTop: $href.offset().top
             }, 500);
