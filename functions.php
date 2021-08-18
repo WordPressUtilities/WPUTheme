8f2be394ba403073fb6ca6c8fb16cfe0a38d88f1
Kevin <kevin@rocher-dev.com>
2021-08-18 12:52:57 +0200

    v 2.94.0
    - New helper : Get oembed infos.

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 require_once dirname(__FILE__) . '/z-protect.php';
 
-define('WPUTHEME_VERSION', '2.93.0');
+define('WPUTHEME_VERSION', '2.94.0');
 
 if (apply_filters('wputheme_usesessions', true)) {
     @session_start();
