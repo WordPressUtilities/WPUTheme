7d561755a9c5517b71e72b8ea16a3b2c681ae49e
Kevin <kevin@rocher-dev.com>
2022-01-26 19:10:59 +0100

    v 2.99.1
    - Sharesheet is now ajax-compatible.

--- js/functions/sharesheet.js
+++ js/functions/sharesheet.js
@@ -1,28 +1,31 @@
-jQuery(document).ready(function($) {
-    var $shareItems = jQuery('[data-share-title],[data-share-url]');
-    if (!$shareItems.length) {
+function wputheme_setup_sharesheet() {
+    if (navigator.share) {
         return;
     }
 
     /* Native sharing is disabled : remove block */
-    if (!navigator.share) {
-        $shareItems.each(function() {
-            var $item = jQuery(this),
-                $parent = $item.parent();
-            if ($parent.children().length == 1) {
-                $parent.remove();
-                return;
-            }
-            $item.remove();
-        });
-        return;
-    }
+    jQuery('[data-share-title],[data-share-url]').each(function() {
+        var $item = jQuery(this),
+            $parent = $item.parent();
+        /* Remove parent node if this one is an only child */
+        if ($parent.children().length == 1) {
+            $parent.remove();
+            return;
+        }
+        $item.remove();
+    });
+}
 
-    $shareItems.on('click', function(e) {
+jQuery(document).ready(function($) {
+    /* Share on click */
+    jQuery('body').on('click', '[data-share-title],[data-share-url]', function(e) {
         e.preventDefault();
         navigator.share({
             title: jQuery(this).attr('data-share-title'),
             url: jQuery(this).attr('data-share-url'),
         });
     });
+    /* Conditionally hide sharesheet  */
+    wputheme_setup_sharesheet();
+    jQuery('body').on('wpu-ajax-ready', wputheme_setup_sharesheet);
 });
