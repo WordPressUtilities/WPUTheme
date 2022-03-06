1c0af07e41b7d6f897a47e7f11d85185143d2523
Kevin <kevin@rocher-dev.com>
2022-03-06 19:51:56 +0100

    v 3.2.3
    - New hook to disable SVG filters.

--- inc/theme/params.php
+++ inc/theme/params.php
@@ -78,6 +78,7 @@ if (apply_filters('wputheme_disable_gutemberg', true)) {
     }, 100);
 
     add_action('after_setup_theme', function () {
+        remove_action('wp_body_open', 'wp_global_styles_render_svg_filters');
         remove_action('wp_enqueue_scripts', 'wp_enqueue_global_styles');
         remove_filter('render_block', 'wp_render_duotone_support');
         remove_filter('render_block', 'wp_render_layout_support_flag');
