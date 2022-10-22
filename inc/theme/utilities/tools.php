d52b8f92f71593c00412ced174230182b2b3c3b6
Kevin <kevin@rocher-dev.com>
2022-10-22 23:09:19 +0200

    v 3.4.8
    - Removed an old class trigerring a notice with PHP 8.1

--- inc/theme/utilities/tools.php
+++ inc/theme/utilities/tools.php
@@ -1,23 +1,6 @@
 <?php
 
 /* ----------------------------------------------------------
-  Fix locale
----------------------------------------------------------- */
-
-/* Thx https://openclassrooms.com/forum/sujet/avoir-la-date-en-francais-grace-a-un-datetime-29453#message-5364823 */
-if (class_exists('DateTime')) {
-    class DateTimeFrench extends DateTime {
-        public function format($format) {
-            $english_days = array('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
-            $french_days = array('Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche');
-            $english_months = array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
-            $french_months = array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
-            return str_replace($english_months, $french_months, str_replace($english_days, $french_days, parent::format($format)));
-        }
-    }
-}
-
-/* ----------------------------------------------------------
   Get current URL
 ---------------------------------------------------------- */
 
