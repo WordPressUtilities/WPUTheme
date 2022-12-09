91d576764398b210224f68980ab2cf8b6d407f00
Kevin <kevin@rocher-dev.com>
2022-12-09 10:50:01 +0100

    v 3.5.1
    Activation :
    - Removed useless settings.

--- inc/theme/activation.php
+++ inc/theme/activation.php
@@ -7,26 +7,13 @@
 add_action('after_switch_theme', 'wputh_setup_theme');
 function wputh_setup_theme() {
 
-    // Options
+    // Default values to avoid unnecessary queries
     $wputh_setup_options = array(
-        'date_format' => 'j F Y',
-        'permalink_structure' => '/%postname%/',
-        'timezone_string' => 'Europe/Paris',
-        'time_format' => 'H:i',
-
-        // Medias
-        'medium_size_w' => '640',
-        'medium_size_h' => '640',
-        'large_size_w' => '1280',
-        'large_size_h' => '1280',
-
-        // Default values to avoid unnecessary queries
         'widget_calendar' => '',
         'widget_nav_menu' => '',
         'widget_pages' => '',
         'widget_post_categories' => '',
-        'widget_tag_cloud' => '',
-        'wpu_home_meta_description' => ''
+        'widget_tag_cloud' => ''
     );
 
     $wputh_setup_options = apply_filters('wputh_setup_options', $wputh_setup_options);
