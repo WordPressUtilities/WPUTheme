fcc8e5ae48de574504ee8fd85a76d551c38be70c
Kevin <kevin@rocher-dev.com>
2023-06-05 17:20:50 +0200

    v 3.7.8
    Prevent empty social links

--- inc/theme/utilities/social.php
+++ inc/theme/utilities/social.php
@@ -33,6 +33,9 @@ function wputh_get_social_links_html($wrapper_classname = 'header__social', $dis
     if (!$wpu_social_links || !is_array($wpu_social_links)) {
         $wpu_social_links = wputh_get_social_links();
     }
+    if (!$wpu_social_links) {
+        return '';
+    }
     $html = '<ul class="wputh-social-links ' . $wrapper_classname . '">';
     foreach ($wpu_social_links as $id => $link) {
         $html .= '<li><a rel="me noreferrer noopener" href="' . $link['url'] . '" class="' . $id . '" title="' . sprintf(__('%s: Follow %s (open in new window)', 'wputh'), $link['name'], get_bloginfo('name')) . '" target="_blank">';
