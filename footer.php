dcf20ba3d52d94b127032db622fc91a573b1a328
Kevin <kevin@rocher-dev.com>
2024-03-12 16:47:43 +0100

    v 3.12.0
    - Better qm timers in header & footer.
    - Cleaner code in header & footer templates.

--- footer.php
+++ footer.php
@@ -2,26 +2,37 @@
 include dirname(__FILE__) . '/z-protect.php';
 do_action('qm/stop', 'wputheme_content');
 do_action('qm/start', 'wputheme_footer');
+
+/* AJAX CHECK */
 do_action('wputheme_main_undercontent_inajax');
 if (defined('IS_AJAX') && IS_AJAX) {
     $current_url = strtok($_SERVER["REQUEST_URI"], '?');
-    echo "<script>if(typeof jQuery !== 'function'){window.location='".$current_url."';}</script>";
+    echo "<script>if(typeof jQuery !== 'function'){window.location='" . $current_url . "';}</script>";
     return;
 }
+
+/* WRAPPER */
 do_action('wputheme_main_undercontent');
 if (apply_filters('wputheme_display_mainwrapper', true)) {
     echo '</div></div>';
-}
-else {
+} else {
     if (apply_filters('wputheme_display_mainwrapper__content', true)):
-    echo '</div>';
+        echo '</div>';
     endif;
 }
+
+/* FOOTER ELEMENTS */
 do_action('wputheme_footer_elements');
 if (apply_filters('wputheme_display_footer', true)) {
     get_template_part('tpl/footer/copyright');
 }
+do_action('qm/stop', 'wputheme_footer');
+
+/* WP FOOTER */
+do_action('qm/start', 'wputheme_wp_footer');
 wp_footer();
+do_action('qm/stop', 'wputheme_wp_footer');
+
+/* END WRAPPER */
 echo '</body>';
 echo '</html>';
-do_action('qm/stop', 'wputheme_footer');
--- header.php
+++ header.php
@@ -1,32 +1,65 @@
 <?php
-require_once dirname( __FILE__ ) . '/z-protect.php';
-do_action('qm/start', 'wputheme_header');
-if (!defined('IS_AJAX') || !IS_AJAX ) {
-?>
-<head><?php echo wp_head(); ?></head>
-<body <?php body_class('cssc-is-responsive'); ?>>
-<?php wp_body_open(); ?>
-<?php do_action('wputheme_header_items'); ?>
-<?php if (apply_filters('wputheme_display_header', true)): ?>
-<div class="main-header centered-container">
-    <header class="banner" id="banner">
-    <?php
-    do_action('wputheme_header_banner');
-    ?>
-    </header>
-</div>
-<?php endif; ?>
-<?php do_action('wputheme_header_elements'); ?>
-<?php if (apply_filters('wputheme_display_mainwrapper', true)): ?>
-<div class="main-container centered-container"><div class="main-container--inner" id="content">
-<?php else: ?>
-<?php if (apply_filters('wputheme_display_mainwrapper__content', true)): ?>
-<div id="content">
-<?php endif; ?>
-<?php endif;
-do_action('wputheme_main_overcontent');
-if (apply_filters('wputheme_display_skiplinks', true)) { echo '<div id="maincontent"></div>'; }
+require_once dirname(__FILE__) . '/z-protect.php';
+if (!defined('IS_AJAX') || !IS_AJAX) {
+
+    /* ----------------------------------------------------------
+      HEAD
+    ---------------------------------------------------------- */
+
+    do_action('qm/start', 'wputheme_wp_head');
+    echo '<head>';
+    wp_head();
+    echo '</head>';
+    do_action('qm/stop', 'wputheme_wp_head');
+
+    /* ----------------------------------------------------------
+      BODY
+    ---------------------------------------------------------- */
+
+    /* BODY CLASS */
+    do_action('qm/start', 'wputheme_body_class');
+    echo '<body ';
+    body_class('cssc-is-responsive');
+    echo '>';
+    do_action('qm/stop', 'wputheme_body_class');
+
+    /* BODY OPEN */
+    do_action('qm/start', 'wputheme_body_open');
+    wp_body_open();
+    do_action('qm/stop', 'wputheme_body_open');
+
+    /* ----------------------------------------------------------
+      HEADER
+    ---------------------------------------------------------- */
+
+    do_action('qm/start', 'wputheme_header');
+    do_action('wputheme_header_items');
+
+    /* HEADER BANNER */
+    if (apply_filters('wputheme_display_header', true)):
+        echo '<div class="main-header centered-container">';
+        echo '<header class="banner" id="banner">';
+        do_action('wputheme_header_banner');
+        echo '</header>';
+        echo '</div>';
+    endif;
+
+    /* MAIN WRAPPER */
+    do_action('wputheme_header_elements');
+    if (apply_filters('wputheme_display_mainwrapper', true)):
+        echo '<div class="main-container centered-container"><div class="main-container--inner" id="content">';
+    else:
+        if (apply_filters('wputheme_display_mainwrapper__content', true)):
+            echo '<div id="content">';
+        endif;
+    endif;
+
+    /* OVER CONTENT */
+    do_action('wputheme_main_overcontent');
+    if (apply_filters('wputheme_display_skiplinks', true)) {
+        echo '<div id="maincontent"></div>';
+    }
+    do_action('qm/stop', 'wputheme_header');
 }
 do_action('wputheme_main_overcontent_inajax');
-do_action('qm/stop', 'wputheme_header');
 do_action('qm/start', 'wputheme_content');
