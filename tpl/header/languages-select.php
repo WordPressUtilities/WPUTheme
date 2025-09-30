a0879ce855c8ea18b35685e78ddd5c573e8dfb39
Kevin <kevin@rocher-dev.com>
2025-09-30 20:14:57 +0200

    v 3.29.4
    avoid duplicate id

--- tpl/header/languages-select.php
+++ tpl/header/languages-select.php
@@ -5,7 +5,7 @@ if (empty($display_languages) || count($display_languages) <= 1) {
     return;
 }
 echo '<div class="switch-lang">';
-echo '<select id="switch-lang-select" name="switch-lang-select" class="languages wpu-pll-lang">';
+echo '<select name="switch-lang-select" class="languages wpu-pll-lang">';
 foreach ($display_languages as $id_lang => $lang) {
     echo '<option ' . ($lang['current'] ? 'selected="selected"' : '') . ' value="' . $lang['url'] . '" data-lang="' . $id_lang . '">' . $lang['name'] . '</option>';
 }
