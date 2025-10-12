16214cf747b917ce702613c02a2d39cca7561045
Kevin <kevin@rocher-dev.com>
2025-10-12 21:42:02 +0200

    v 3.32.1
    Summary fixes
    - Hide wrapper if no summary
    - Links now always have a unique & valid ID.

--- js/functions/summary.js
+++ js/functions/summary.js
@@ -1,22 +1,31 @@
-
 /* ----------------------------------------------------------
   Build a summary of links
 ---------------------------------------------------------- */
 
-function wputheme_build_summary($headings, $links_list) {
+function wputheme_build_summary($headings, $links_list, $wrapper) {
     'use strict';
     if (!$headings || !$links_list) {
         return false;
     }
-    /* $headings - i */
+
+    var _has_summary = false;
     for (var i = 0, len = $headings.length; i < len; i++) {
-        wputheme_build_summary_item($headings[i], i, $links_list);
+        if (wputheme_build_summary_item($headings[i], i, $links_list)) {
+            _has_summary = true;
+        }
+    }
+
+    if ($wrapper) {
+        $wrapper.setAttribute('data-has-summary', _has_summary ? 'true' : 'false');
+        if (!_has_summary) {
+            $wrapper.style.display = 'none';
+        }
     }
 }
 
 function wputheme_build_summary_item($item, i, $links_list) {
     if (!wputheme_is_element_visible($item)) {
-        return;
+        return false;
     }
 
     /* Add an ID to this block */
@@ -24,7 +33,10 @@ function wputheme_build_summary_item($item, i, $links_list) {
     if ($item.id) {
         unique_id = $item.id;
     } else {
-        unique_id = $item.textContent.replace(/\s+/g, '-').toLowerCase() + '-' + i;
+        unique_id = $item.textContent.replace(/[^a-z0-9\-]+/gi, '-').toLowerCase() + '-' + i;
+        if (document.getElementById(unique_id)) {
+            unique_id += '-' + Math.random().toString(36).substring(2, 10);
+        }
         $item.id = unique_id;
     }
 
@@ -35,4 +47,6 @@ function wputheme_build_summary_item($item, i, $links_list) {
     link.textContent = $item.textContent;
     list_item.appendChild(link);
     $links_list.appendChild(list_item);
+
+    return true;
 }
