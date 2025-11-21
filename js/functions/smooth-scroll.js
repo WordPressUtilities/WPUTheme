8fccfea78e100cd30cf09aeec920d779ac8c1a68
Kevin <kevin@rocher-dev.com>
2025-11-21 12:05:43 +0100

    v 3.33.1
    Smooth scroll : handle mobile offset

--- js/functions/smooth-scroll.js
+++ js/functions/smooth-scroll.js
@@ -3,6 +3,22 @@
 ---------------------------------------------------------- */
 
 var wpu_set_smooth_scroll = function() {
+
+    var desktop_offset = 0;
+    if (window.wputheme_smooth_scroll_offset !== undefined) {
+        desktop_offset = parseInt(window.wputheme_smooth_scroll_offset, 10);
+    }
+
+    var mobile_offset = desktop_offset;
+    if (window.wputheme_smooth_scroll_mobile_offset !== undefined) {
+        mobile_offset = parseInt(window.wputheme_smooth_scroll_mobile_offset, 10);
+    }
+
+    var mobile_offset_breakpoint = 768;
+    if (window.wputheme_smooth_scroll_mobile_offset_breakpoint !== undefined) {
+        mobile_offset_breakpoint = parseInt(window.wputheme_smooth_scroll_mobile_offset_breakpoint, 10);
+    }
+
     /* Scrollto */
     jQuery('body').on('click', '[href^="#"]:not(.no-smooth)', function(e) {
         var href = jQuery(this).attr('href');
@@ -10,10 +26,7 @@ var wpu_set_smooth_scroll = function() {
             return;
         }
         var $href = jQuery(href);
-        var _offset = 0;
-        if(window.wputheme_smooth_scroll_offset !== undefined){
-            _offset = parseInt(window.wputheme_smooth_scroll_offset, 10);
-        }
+        var _offset = (window.innerWidth <= mobile_offset_breakpoint) ? mobile_offset : desktop_offset;
         if ($href.length > 0) {
             e.preventDefault();
             jQuery('html,body').animate({
