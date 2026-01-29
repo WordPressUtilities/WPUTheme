ed81fd140b0d971d8aa890a7b1e7c284d0be7c5c
Kevin <kevin@rocher-dev.com>
2026-01-29 21:29:46 +0100

    v 3.35.5
    fix for default template.

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 include __DIR__ . '/z-protect.php';
 get_header();
-
 echo '<div class="main-content">';
-echo get_the_loop();
+echo function_exists('get_the_loop') ? get_the_loop() : 'No content available.';
 echo '</div>';
 get_sidebar();
 get_footer();
